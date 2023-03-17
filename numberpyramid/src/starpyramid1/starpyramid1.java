d66c2ec584f867218c60f49ba6cf480a19d99dd2
Vishal Pansare <51355863+vishalpansare5600@users.noreply.github.com>
2023-03-17 11:58:51 +0530

    Add files via upload

---
+++ numberpyramid/src/starpyramid1/starpyramid1.java
@@ -0,0 +1,20 @@
+package starpyramid1;
+
+public class starpyramid1 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		int i,j;
+		for(i=1 ; i<=5 ;i++)
+		{
+			for(j=1 ;j<=i;j++)
+			{
+				System.out.print(j+"  ");
+			}
+				System.out.println("  ");
+			}
+		}
+}
+
